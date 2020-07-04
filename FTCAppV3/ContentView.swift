13e29a29af3bafe8bea1f6dfcd3a070245bc60a1
Roohit Ravi <rohitravi2001@gmail.com>
2020-07-03 22:31:48 -0700

    Initial Commit

---
+++ FTCAppV3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FTCAppV3
+//
+//  Created by Rohit Ravi on 7/3/20.
+//  Copyright © 2020 Rohit Ravi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
